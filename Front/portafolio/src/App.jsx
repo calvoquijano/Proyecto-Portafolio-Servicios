5ea6ad401c92ee32d06282e76c17e03065e88b20
calvoquijano <calvoquijano@hotmail.com>
2023-07-10 15:13:50 -0500

    estilos generales

--- Front/portafolio/src/App.jsx
+++ Front/portafolio/src/App.jsx
@@ -1,32 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
+      <h2>Vite + React</h2>
+      <h3>Vite + React</h3>
+      <h4>Vite + React</h4>
+      <h5>Vite + React</h5>
+      <h6>Vite + React</h6>
+      
+      <p>
         Click on the Vite and React logos to learn more
+      </p>
+      <p>
+        <b> Click on the Vite and React logos to learn more</b>
+      </p>
+      <p>
+        <em> Click on the Vite and React logos to learn more</em>
+      </p>
+      <p>
+        <i> Click on the Vite and React logos to learn more</i>
+      </p>
+      <p>
+        <a href=''> Click on the Vite and React logos to learn more</a>
+      </p>
+      <p>
+        <a href='' className='link--secundario'> Click on the Vite and React logos to learn more</a>
+      </p>
+      <p>
+        <a href='' className='link--ok'> Click on the Vite and React logos to learn more</a>
+      </p>
+      <p>
+        <a href='' className='link--cancelar'> Click on the Vite and React logos to learn more</a>
       </p>
     </>
   )
