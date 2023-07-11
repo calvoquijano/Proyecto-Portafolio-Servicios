f291fce268cf99ec27f5e642ddab830a6488f2a3
Andrés "El Calvo" Quijano <91165034+calvoquijano@users.noreply.github.com>
2023-07-11 05:34:38 -0500

    Hice cambios en el css y añadí json

--- Front/portafolio/src/Components/Pages/Home/SobreMi/SobreMi.jsx
+++ Front/portafolio/src/Components/Pages/Home/SobreMi/SobreMi.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import styles from "./SobreMi.module.css";
+import { Box, Grid } from "@mui/material";
 
 const SobreMi = () => {
   return (
-    <div>SobreMi</div>
-  )
-}
+    <>
+      <Box sx={{ flexGrow: 1 }} className={styles.container}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            Diseñador UX / UI y Desarrollador Front-end en JavaScript con React.
+          </Grid>
+          <Grid item xs={6}>
+            <img className={styles.imagenes} src="https://calvoquijano.com/images/headers/home.png" alt="Perfil página de inicio" />
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};
 
-export default SobreMi
+export default SobreMi;
